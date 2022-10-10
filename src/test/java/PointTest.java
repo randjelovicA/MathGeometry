7939fef4c4a22f51188c328443f9a889276bc040
Aleksa Randjelovic <10219aleksarandjelovic@etsntesla.edu.rs>
2022-10-10 12:23:04 +0200

    -m test

--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -1,29 +1,21 @@
 import org.etsntesla.it.math.Point;
+import org.junit.jupiter.api.AssertionFailureBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class PointTest {
-    static Point a=new Point();
-    static Point b=new Point();
-
+    static Point A,B;
     @BeforeAll
     static void init(){
-        a.setX(3);
-        a.setY(4);
-        b.setX(3);
-        b.setY(6);
+        A = new Point(3,4);
+        B = new Point(3,6);
+    }
+    @Test
+    void test1(){
+        System.out.println("Hello test");
+        Assertions.assertEquals(5,A.radius(),"Formula nije tacna!");
+        Assertions.assertEquals(2,A.distance(B),"Formula nije tacna!");
     }
 
-    @Test
-    public void test1(){
-        Point t=new Point(3,4);
-        Assertions.assertEquals(5.0d,t.radius(),"Greska! Uneta pogresna ocekivana vrednost.");
-    }
-
-    @Test
-    public void test2(){
-        Assertions.assertEquals(5.0d,a.radius(),"Greska! Uneta pogresna ocekivana vrednost.");
-        //Assertions.fail();
-    }
 }
